7b96e650042c96b606c3044b744ec0e0373e3993
Rohil Muktibodh <103750236+rohilm@users.noreply.github.com>
2025-05-25 13:14:48 +0530

    Initial commit

---
+++ models/schooldata/school_sum.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='view') }}
+
+SELECT
+  states_union_territories AS state,
+  SUM("Primary Schools") AS total_primary_schools,
+  SUM("Upper Primary Schools") AS total_upper_primary_schools,
+  SUM("Pre-Primary Schools") AS total_pre_primary_schools,
+  SUM("High/ Secondary Schools") AS total_high_secondary_schools
+FROM {{ source('public', 'schooldata') }}
+GROUP BY states_union_territories
